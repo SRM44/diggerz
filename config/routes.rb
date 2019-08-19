280d956037755ad55bb1c33faa53538bd0db9b12
Steven Rigolet <steven-r@hotmail.fr>
2019-08-19 19:10:27 +0200

    schema ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+
+  resources :deals, only: [:index, :show] do
+    member do
+      patch :confirm
+      patch :accept
+      patch :decline
+    end
+  end
+
+  resources :records, only: [:index, :show] do
+    resources :deals, only: [:new, :create]
+  end
+
+  resources :myrecords, only: [:index, :show, :new, :create] do
+    member do
+      patch :toggle_swappable
+    end
+  end
+
+  resource :profile, only: [:edit, :update]
+
 end
