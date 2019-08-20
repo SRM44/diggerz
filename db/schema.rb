8db8a63b2cf72d40313db2c6a3cfe83cb092dd48
CodeWoman29 <46631000+CodeWoman29@users.noreply.github.com>
2019-08-20 18:08:07 +0200

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,6 @@ ActiveRecord::Schema.define(version: 2019_08_19_165115) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "cocktails", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "deals", force: :cascade do |t|
     t.string "status"
     t.datetime "created_at", null: false
