3beb36c5f82fb3aa049d98ceb13e3901c1ded28f
Volodia Lepron <dzatoem@gmail.com>
2019-08-20 11:14:13 +0200

    add associations to models

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,2 +1,4 @@
 class Deal < ApplicationRecord
+  belongs_to :receiver_record, class_name: 'Record'
+  belongs_to :requester_record, class_name: 'Record'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :records
+  has_many :preferences
+  has_many :genres, through: :preferences
+
+  def received_deals
+    Deal.where(receiver_record_id: record_ids)
+  end
+
+  def requested_deals
+    Deal.where(requester_record_id: record_ids)
+  end
 end
