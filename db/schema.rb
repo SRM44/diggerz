28ead4d7ce5edff9b3d331767bdb6ce6af05638d
Volodia Lepron <dzatoem@gmail.com>
2019-08-20 11:17:52 +0200

    migrate after new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_171558) do
+ActiveRecord::Schema.define(version: 2019_08_20_091241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "deals", force: :cascade do |t|
-    t.string "status"
+    t.string "status", default: "pending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "requester_record_id"
