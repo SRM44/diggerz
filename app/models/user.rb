29bf30f44a09c508b0988f5cf3399c310d8c2e3e
Amelie CARO <amelie.caro@orange.fr>
2019-08-20 17:37:00 +0200

    Page de login intégrant la connexion via ses identifiants Discogs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,28 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
+
+  def self.find_for_discogs_oauth(auth)
+    user_params = {
+      username:   auth.info.username,
+      avatar:     auth.info.picture,
+      email:      auth.info.username.downcase + '@diggerz-user.com',
+      discogs_id: auth.uid,
+      provider:   auth.provider,
+      token:      auth.credentials.token
+    }
+
+    user = User.find_by(provider: auth.provider, discogs_id: auth.uid)
+    user ||= User.find_by(email: user_params["email"]) # User did a regular sign up in the past.
+
+    if user
+      user.update(user_params)
+    else
+      user = User.new(user_params)
+      user.password = Devise.friendly_token[0,20]  # Fake password for validation
+      user.save!
+    end
+    return user
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_171558) do
+ActiveRecord::Schema.define(version: 2019_08_20_151158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,9 @@ ActiveRecord::Schema.define(version: 2019_08_19_171558) do
     t.string "username"
     t.string "name"
     t.string "location"
+    t.string "provider"
+    t.string "discogs_id"
+    t.string "token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
