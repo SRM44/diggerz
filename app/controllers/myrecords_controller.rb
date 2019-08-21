26681b31d293342db6311c30166ff11d66a9c886
Amelie CARO <amelie.caro@orange.fr>
2019-08-21 19:04:08 +0200

    Correction d'un paramètre de la connection à discogs

--- app/controllers/myrecords_controller.rb
+++ app/controllers/myrecords_controller.rb
@@ -1,14 +1,14 @@
 class MyrecordsController < ApplicationController
   def index
-    @records = current_user.records
+    @records = current_user.records.includes(:release)
   end
 
   def import_from_discogs
-    @discogs = Discogs::Wrapper.new("Diggerz", session[:access_token])
+    @discogs = Discogs::Wrapper.new("Diggerz", access_token: session[:access_token])
 
     ImportCollectionFromDiscogsService.new(current_user, @discogs).call
 
-    # redirect_to myrecords_path, notice: "Succesfully imported Discogs' collection"
+    redirect_to myrecords_path, notice: "Succesfully imported Discogs' collection"
     # <%= link_to 'Importer ma collection', import_from_discogs_releases_path %>
   end
 end
