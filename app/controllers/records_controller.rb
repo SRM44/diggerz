c30e3599be7662ee36aae0d211ad972b19f3cd1e
Volodia Lepron <dzatoem@gmail.com>
2019-08-22 00:22:57 +0200

    Add discover page before js

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,10 +1,10 @@
 class RecordsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
-
   def index
     @records = Record.all
-    @discover = @records.each do |record|
-      return record if record.user != current_user && record.swappable && current_user.genres.include?(record.genre)
+    @record = Record.first
+    @discover = []
+    @records.each do |record|
+      @discover << record if record.user != current_user && record.swappable && current_user.genres.include?(record.release.genre)
     end
   end
 end
