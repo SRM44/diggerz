4e1e8e0f075eefd4472221df78aafd59798e2f0f
Volodia Lepron <dzatoem@gmail.com>
2019-08-22 14:50:03 +0200

    correct genre on discover

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,7 +3,8 @@ class RecordsController < ApplicationController
     @records = Record.all
     @discover = []
     @records.each do |record|
-      @discover << record if record.user != current_user && record.swappable && current_user.genres.include?(record.release.genre)
+      @discover << record if record.user != current_user && record.swappable
+      # && current_user.genres.include?(record.release.genre)
     end
   end
 end
