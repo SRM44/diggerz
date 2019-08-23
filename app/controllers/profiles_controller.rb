97d66c2df4bf51f1e05c15272342cecee9ac1014
Amelie CARO <amelie.caro@orange.fr>
2019-08-23 15:11:48 +0200

    Import données du profil + affichage de l'avatar dans le profil

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,21 @@
+class ProfilesController < ApplicationController
+  before_action :set_discogs, only: [:show, :import_from_discogs]
+
+  def show
+    unless current_user.avatar
+      ImportFromDiscogsService.new(@discogs, user: current_user).import_user_data
+      redirect_to profile_path
+    end
+    @user = current_user
+  end
+
+  def import_from_discogs
+    imported = ImportFromDiscogsService.new(@discogs, user: current_user).import_user_data
+  end
+
+  private
+
+  def set_discogs
+    @discogs = Discogs::Wrapper.new("Diggerz", access_token: session[:access_token])
+  end
+end
