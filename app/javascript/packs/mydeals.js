b9ce7a44ed1ae7bd870be4344fb9890a1031c318
Steven Rigolet <steven-r@hotmail.fr>
2019-08-26 18:54:34 +0200

    js custom page

--- app/javascript/packs/mydeals.js
+++ app/javascript/packs/mydeals.js
@@ -2,10 +2,13 @@ $("#md-sub-one").click(function () {
 
     $("#md-section-one").show();
     $("#md-section-two").hide();
+    $(this).toggleClass('active-deals').siblings().removeClass('active-deals');
+
 });
+
 $("#md-sub-two").click(function () {
 
     $("#md-section-two").show();
     $("#md-section-one").hide();
-
+    $(this).toggleClass('active-deals').siblings().removeClass('active-deals');
 });
