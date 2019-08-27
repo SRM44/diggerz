2a10141381f9161ab7798c337116506ea376e3d8
Steven Rigolet <steven-r@hotmail.fr>
2019-08-27 19:08:58 +0200

    new deals cards design

--- app/controllers/mydeals_controller.rb
+++ app/controllers/mydeals_controller.rb
@@ -1,6 +1,6 @@
 class MydealsController < ApplicationController
   def index
-    @received_deals = current_user.received_deals
+    @received_deals = current_user.received_deals.with_status(:pending)
     @sended_deals = current_user.requested_deals.with_status(:pending)
     @accepted_deals = current_user.received_deals.with_status(:accepted) || current_user.requested_deals.with_status(:accepted)
     @confirmed_deals = current_user.requested_deals.with_status(:confirmed)
