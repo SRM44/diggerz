a58a685d14987284529b9d8fb5e20e3297b831c3
Steven Rigolet <steven-r@hotmail.fr>
2019-08-28 15:01:36 +0200

    my deals page kind of okay and my deals show is okay

--- app/controllers/mydeals_controller.rb
+++ app/controllers/mydeals_controller.rb
@@ -1,4 +1,6 @@
 class MydealsController < ApplicationController
+  before_action :set_deal, only: [:show]
+
   def index
     @received_deals = current_user.received_deals.with_status(:pending)
     @sended_deals = current_user.requested_deals.with_status(:pending)
@@ -7,5 +9,13 @@ class MydealsController < ApplicationController
   end
 
   def show
+    @proposed_record = @deal.requester_record
+    @asked_record = @deal.receiver_record
+  end
+
+  private
+
+  def set_deal
+    @deal = Deal.find(params[:id])
   end
 end
