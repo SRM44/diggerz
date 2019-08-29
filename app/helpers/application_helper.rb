2814b8edd3bcffe7a2fd9408dd58f3f922efbe13
Yanis Chaigneau <yanis.chaigneau4@orange.fr>
2019-08-29 18:11:02 +0200

    modification show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def current_user_avatar_path
     if current_user.avatar.url.nil?
-      image_path 'default_profile.jpg'
+      image_path 'default_profile.png'
     else
       cl_image_path current_user.avatar
     end
