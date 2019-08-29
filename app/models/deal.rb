ed422e0c35cb6885d4491985612095e009f478cc
Volodia Lepron <dzatoem@gmail.com>
2019-08-29 19:07:43 +0200

    fix mydeals controller

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,8 +1,4 @@
 class Deal < ApplicationRecord
-  extend Enumerize
-
   belongs_to :receiver_record, class_name: 'Record'
   belongs_to :requester_record, class_name: 'Record'
-
-  enumerize :status, in: [:pending, :accepted, :refused, :confirmed], scope: true
 end
