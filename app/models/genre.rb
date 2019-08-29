251633ade05344dbd47ce81fee2a897186d6d293
Amelie CARO <amelie.caro@orange.fr>
2019-08-29 18:03:09 +0200

    Ajout images aux genres + changement d'une ligne de code pour supprimer les caractères spéciaux des noms de genre

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,8 +1,23 @@
 class Genre < ApplicationRecord
+  SAVED_IMAGE_GENRES = [
+    "blues",
+    "brass-military",
+    "children-s",
+    "classical",
+    "electronic",
+    "folk-world-country",
+    "funk-soul",
+    "jazz",
+    "latin",
+    "non-music",
+    "pop",
+    "reggae",
+    "rock",
+    "stage-screen"
+  ]
+
   has_many :users, through: :preferences
   has_many :releases
 
   validates :name, uniqueness: true
-
-  SAVED_IMAGE_GENRES = ["blues", "folk", "jazz", "pop", "reggae", "rock", "electronic", "classical", "funksoul"]
 end
