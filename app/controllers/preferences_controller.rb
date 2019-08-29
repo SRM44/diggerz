5d8cf349ace44e8d92b065c7f8797b832515e27e
Amelie CARO <amelie.caro@orange.fr>
2019-08-29 11:24:01 +0200

    Correction page genre pour garder en mémoire les préférences de genre des users

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -2,12 +2,15 @@ class PreferencesController < ApplicationController
 
   def edit
     @genres = Genre.all
+    @user_genres = current_user.genres.to_a # prevent call to ActiveRecord#include?
   end
 
   def update
-    current_user.preferences.destroy_all
-    params[:genre_id].each do | id |
-      current_user.preferences.create(genre_id: id)
+    # current_user.preferences.destroy_all
+    # params[:genre_id].each do | id |
+    #   current_user.preferences.update(genre_id: id)
+    # end
+    current_user.update(genre_ids: params[:genre_id])
+    redirect_to edit_profile_preferences_path
     end
-  end
 end
