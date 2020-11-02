e64718e020763f48b1e8420c62a3050c2f974330
Steven Rigolet <steven-r@hotmail.fr>
2020-11-02 22:08:28 +0100

    Some DRY on JS flow.

--- app/javascript/packs/deal.js
+++ app/javascript/packs/deal.js
@@ -1,53 +1,68 @@
 /* 
-For each records of my collection 
-On click
-Add record to deal and close my collection
-
-++
-
 For my proposed record 
 On click 
-Add a different record to deal and close
+Check for previous record
+Select a record within my collection
+Remove previous record 
+Add the new record to deal
+Close my collection
 */
 
+/* SELECT MY RECORD */
 const records = document.querySelectorAll('.deal-myrecord')
 
 records.forEach((record) => {
   record.addEventListener('click', () => {
-
+    
     const requesterRecord = document.getElementById('deal_requester_record_id')
     requesterRecord.value = record.dataset.recordId
-
-    const dealBtn = document.querySelector('.deal-input')
-    dealBtn.classList.add('deal-input-show')
-    dealBtn.classList.remove('deal-input')
-
-    const resultDiv     = document.getElementById('deal-myrecord-swapped')
-
-    const resultContent = `
-      <div class="deal-myrecord-swapped-infos">
-        <p><em>En échange de :</em></p>
-        <h4>${record.dataset.recordTitle}</h4>
-        <p>${record.dataset.recordArtist}</p>
-      </div>
-      <div class="deal-myrecord-swapped-image">
-        <img class="avatar-square" src="${record.dataset.recordImage}">
-      </div>
-    `
-
-    resultDiv.insertAdjacentHTML('afterbegin', resultContent)
+    
+    if (addMyRecord.called) { 
+      console.log('coucou');
+    } else { 
+      recordInput();
+      addMyRecord(record);
+      modifyBtn();
+    }
   })
 })
 
-/* OPEN MYRECORDS COLLECTION */
-const displayButton = document.getElementById('vinyl-card-proposition-button')
-const closeButton = document.getElementById('close_collection_button')
+/* MODIFY SHOW INPUT */
+function recordInput() {
+  document.querySelector('.deal-input').classList.add('deal-input-show')
+}
 
-displayButton.addEventListener('click', toggleCollection);
-closeButton.addEventListener('click', toggleCollection);
+/* MODIFY COLLECTION BTN */
+function modifyBtn() {
+  document.getElementById("vinyl-card-proposition-button").innerHTML =" Modifier le disque";
+}
+
+/* ADD MY RECORD TO RESULTS */
+function addMyRecord(record, presence) {
+  const resultDiv = document.getElementById('deal-myrecord-swapped')
+
+  
+  const resultContent = `
+    <div class="deal-myrecord-swapped-infos">
+      <p><em>En échange de :</em></p>
+      <h4>${record.dataset.recordTitle}</h4>
+      <p>${record.dataset.recordArtist}</p>
+    </div>
+    <div class="deal-myrecord-swapped-image">
+      <img class="avatar-square" src="${record.dataset.recordImage}">
+    </div>
+  `
+  resultDiv.insertAdjacentHTML('afterbegin', resultContent)
+  toggleCollection();
+}
+
+
+/* TOGGLE MYRECORDS COLLECTION */
+const displayButtons = document.querySelectorAll('#vinyl-card-proposition-button')
+displayButtons.forEach((button) => {
+  button.addEventListener('click', toggleCollection);
+})
 
 function toggleCollection() {
-  const displayButton = document.querySelector('.vinyl-card-collection')
-  
-  displayButton.classList.toggle('records-undisplay');
+  document.querySelector('.vinyl-card-collection').classList.toggle('records-undisplay');
 }
