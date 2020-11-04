cf68b6a6e446bd15ff6a785512da795e2001fb56
Steven Rigolet <steven-r@hotmail.fr>
2020-11-04 16:41:49 +0100

    Deals flow is almost completed.
    Design will come at the very end. But back end dev is able to integrate logics from the moment a deal is validated.

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,5 +1,5 @@
 class DealsController < ApplicationController
-  skip_before_action :redirect_user_without_confirmed_email!, only: [:new, :create, :index, :show, :import_from_discogs]
+  skip_before_action :redirect_user_without_confirmed_email!, only: [:new, :create, :index, :show, :accept, :decline, :import_from_discogs]
 
   def new
     @record = Record.find(params[:record_id])
