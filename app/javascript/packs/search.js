0ebe5f4e81fd93ffc79d6ac169347ea7d19fc9f1
Steven Rigolet <steven-r@hotmail.fr>
2020-11-10 20:29:29 +0100

    navbar upgrades.
    Some improvement on the navbar design and responsivness for desktop UX.

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,17 +1,25 @@
 const recordsCards = document.querySelectorAll('.records-index-card');
 
+function undisplay(record) {
+  record.classList.toggle('records-undisplay');
+}
+
 recordsCards.forEach(recordCard => {
-    const recordModal = document.getElementById('discover-record-modal-' + recordCard.dataset.recordId)
-  
-    recordCard.addEventListener('click', (event) => {
-      recordModal.classList.toggle('records-undisplay')
-    })
-  
-    const close = recordModal.querySelector('#record-close-modal-' + recordCard.dataset.recordId)
-    
-    close.addEventListener('click', () => {
-      recordModal.classList.toggle('records-undisplay')
-    })
-  
+  const recordModal = document.getElementById('discover-record-modal-' + recordCard.dataset.recordId)
+  const close = recordModal.querySelector('#record-close-modal-' + recordCard.dataset.recordId)
+
+  recordCard.addEventListener('click', (event) => {
+    undisplay(recordModal);
   })
+
+  close.addEventListener('click', () => {
+    undisplay(recordModal);
+  })
+})
   
+    /* 
+  If a single record element does not contain undisplay 
+  Toggle function close 
+  Then toggle undisplay on clicked element 
+  Else just toggle undisplay on clicked element 
+  */
