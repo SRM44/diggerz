d07a4d754596dd5482c6d016532963d7c346715f
Steven Rigolet <steven-r@hotmail.fr>
2020-11-11 11:25:39 +0100

    Records index page is ready. Just miss one JS fix, but the bêta version is ready to go, both desktop and mobile.

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -4,10 +4,14 @@ function undisplay(record) {
   record.classList.toggle('records-undisplay');
 }
 
+function checkRecord(record) {
+  record.querySelector("div:not(.records-undisplay)");
+}
+
 recordsCards.forEach(recordCard => {
   const recordModal = document.getElementById('discover-record-modal-' + recordCard.dataset.recordId)
   const close = recordModal.querySelector('#record-close-modal-' + recordCard.dataset.recordId)
-
+  
   recordCard.addEventListener('click', (event) => {
     undisplay(recordModal);
   })
@@ -18,7 +22,8 @@ recordsCards.forEach(recordCard => {
 })
   
     /* 
-  If a single record element does not contain undisplay 
+  
+    Check THE record element that does not contain undisplay 
   Toggle function close 
   Then toggle undisplay on clicked element 
   Else just toggle undisplay on clicked element 
