c277fe8570e2f11b55b0556e75080b0111a8f54f
Steven Rigolet <steven-r@hotmail.fr>
2020-11-25 09:15:08 +0100

    commit merge

--- app/javascript/packs/discover.js
+++ app/javascript/packs/discover.js
@@ -4,34 +4,34 @@ initAnim(cards);
 
 function initAnim(array) {
   if(array.length >= 4 ) {
-    TweenMax.fromTo(array[0], 0, {x:0, y: 0, opacity:0.5}, {x:0, y: -120, opacity:0, zIndex: 0, delay:0.01, ease: Cubic.easeInOut });
+    gsap.fromTo(array[0], 0, {x:0, y: 0, opacity:0.5}, {x:0, y: -120, opacity:0, zIndex: 0, delay:0.01, ease: Cubic.easeInOut });
 
-    TweenMax.fromTo(array[1], 0, {x:0, y: 50, opacity:1, zIndex: 1}, {x:0, y: 0, opacity:0.5, zIndex: 0, ease: Cubic.easeInOut});
+    gsap.fromTo(array[1], 0, {x:0, y: 50, opacity:1, zIndex: 1}, {x:0, y: 0, opacity:0.5, zIndex: 0, ease: Cubic.easeInOut});
 
-    TweenMax.to(array[2], 0, {bezier:[{x:0, y:100}, {x:65, y:75}, {x:0, y:50}], zIndex: 1, opacity: 1, ease: Cubic.easeInOut});
+    gsap.to(array[2], 0, {bezier:[{x:0, y:100}, {x:65, y:75}, {x:0, y:50}], zIndex: 1, opacity: 1, ease: Cubic.easeInOut});
 
-    TweenMax.fromTo(array[3], 0, {x:0, y:400, opacity: 0, zIndex: 0}, {x:0, y:80, opacity: 0.5, zIndex: 0, ease: Cubic.easeInOut});
+    gsap.fromTo(array[3], 0, {x:0, y:400, opacity: 0, zIndex: 0}, {x:0, y:80, opacity: 0.5, zIndex: 0, ease: Cubic.easeInOut});
   }
 }
 
 function startAnim(array) {
-  TweenMax.fromTo(array[0], 0.5, {x:0, y: 0, opacity:0.5}, {x:0, y: -120, opacity:0, zIndex: 0, delay:0.01, ease: Cubic.easeInOut });
+  gsap.fromTo(array[0], 0.5, {x:0, y: 0, opacity:0.5}, {x:0, y: -120, opacity:0, zIndex: 0, delay:0.01, ease: Cubic.easeInOut });
 
-  TweenMax.fromTo(array[1], 0.5, {x:0, y: 50, opacity:1, zIndex: 1}, {x:0, y: 0, opacity:0.5, zIndex: 0, ease: Cubic.easeInOut});
+  gsap.fromTo(array[1], 0.5, {x:0, y: 50, opacity:1, zIndex: 1}, {x:0, y: 0, opacity:0.5, zIndex: 0, ease: Cubic.easeInOut});
 
-  TweenMax.to(array[2], 0.5, {bezier:[{x:0, y:100}, {x:65, y:75}, {x:0, y:50}], zIndex: 1, opacity: 1, ease: Cubic.easeInOut});
+  gsap.to(array[2], 0.5, {bezier:[{x:0, y:100}, {x:65, y:75}, {x:0, y:50}], zIndex: 1, opacity: 1, ease: Cubic.easeInOut});
 
-  TweenMax.fromTo(array[3], 0.5, {x:0, y:400, opacity: 0, zIndex: 0}, {x:0, y:100, opacity: 0.5, zIndex: 0, ease: Cubic.easeInOut});
+  gsap.fromTo(array[3], 0.5, {x:0, y:400, opacity: 0, zIndex: 0}, {x:0, y:100, opacity: 0.5, zIndex: 0, ease: Cubic.easeInOut});
 }
 
 function reverseAnim(array) {
-  TweenMax.fromTo(array[0], 0.5, {x:0, y: -120, opacity:0}, {x:0, y: 0, opacity:0.5, zIndex: 0, delay:0.01, ease: Cubic.easeInOut });
+  gsap.fromTo(array[0], 0.5, {x:0, y: -120, opacity:0}, {x:0, y: 0, opacity:0.5, zIndex: 0, delay:0.01, ease: Cubic.easeInOut });
 
-  TweenMax.to(array[1], 0.5, {bezier:[{x:0, y:0}, {x:65, y:25}, {x:0, y:50}], zIndex: 1, opacity: 1, ease: Cubic.easeInOut});
+  gsap.to(array[1], 0.5, {bezier:[{x:0, y:0}, {x:65, y:25}, {x:0, y:50}], zIndex: 1, opacity: 1, ease: Cubic.easeInOut});
 
-  TweenMax.fromTo(array[2], 0.5, {x:0, y: 50, opacity:1, zIndex: 1}, {x:0, y: 100, opacity:0.5, zIndex: 0, ease: Cubic.easeInOut});
+  gsap.fromTo(array[2], 0.5, {x:0, y: 50, opacity:1, zIndex: 1}, {x:0, y: 100, opacity:0.5, zIndex: 0, ease: Cubic.easeInOut});
 
-  TweenMax.fromTo(array[3], 0.5, {x:0, y:100, opacity: 0.5, zIndex: 0}, {x:0, y:400, opacity: 0, zIndex: 0, ease: Cubic.easeInOut});
+  gsap.fromTo(array[3], 0.5, {x:0, y:100, opacity: 0.5, zIndex: 0}, {x:0, y:400, opacity: 0, zIndex: 0, ease: Cubic.easeInOut});
 }
 
 function nextArray(array) {
