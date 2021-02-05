9ee7c9f1fa3dd55156a7c0d0af7c3a3525192f5a
Nicolas Filzi <nfilzi.webservices@gmail.com>
2021-02-05 12:20:18 +0100

    Scope down available cities to NANTES

--- app/models/user/location.rb
+++ app/models/user/location.rb
@@ -2,40 +2,44 @@ class User
   class Location
     def self.available
       %w(
-          AIX-EN-PROVENCE
-          AMIENS
-          ANGERS
-          BESANCONS
-          BORDEAUX
-          BREST
-          CAEN
-          CLERMONT-FERRAND
-          DIJON
-          GRENOBLE
-          LE\ HAVRE
-          LE\ MANS
-          LILLE
-          LIMOGES
-          LYON
-          MARSEILLE
-          METZ
-          MONTPELLIER
-          MULHOUSE
           NANTES
-          NICE
-          NIMES
-          ORLEANS
-          PARIS
-          PERPIGNAN
-          REIMS
-          RENNES
-          ROUEN
-          SAINT-ETIENNE
-          STRASBOURG
-          TOULON
-          TOURS
-          VILLEURBANNES
         )
+
+      # %w(
+      #     AIX-EN-PROVENCE
+      #     AMIENS
+      #     ANGERS
+      #     BESANCONS
+      #     BORDEAUX
+      #     BREST
+      #     CAEN
+      #     CLERMONT-FERRAND
+      #     DIJON
+      #     GRENOBLE
+      #     LE\ HAVRE
+      #     LE\ MANS
+      #     LILLE
+      #     LIMOGES
+      #     LYON
+      #     MARSEILLE
+      #     METZ
+      #     MONTPELLIER
+      #     MULHOUSE
+      #     NANTES
+      #     NICE
+      #     NIMES
+      #     ORLEANS
+      #     PARIS
+      #     PERPIGNAN
+      #     REIMS
+      #     RENNES
+      #     ROUEN
+      #     SAINT-ETIENNE
+      #     STRASBOURG
+      #     TOULON
+      #     TOURS
+      #     VILLEURBANNES
+      #   )
     end
   end
 end
