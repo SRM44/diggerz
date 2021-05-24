fcff5257144634113508ff074c404996b9d9b7ff
Nicolas Filzi <nfilzi.webservices@gmail.com>
2021-05-24 11:21:48 +0200

    Prevent preview pic JS from running if no input found

--- app/javascript/packs/preview_pictures.js
+++ app/javascript/packs/preview_pictures.js
@@ -1,19 +1,20 @@
 const input = document.getElementById('photo-input');
 
-input.addEventListener('change', () => {
-  displayPreview(input);
-})
-
+if (input) {
+  input.addEventListener('change', () => {
+    displayPreview(input);
+  })
+}
 
 function displayPreview(input) {
-    if (input.files && input.files[0]) {
-      const reader = new FileReader();
-      const preview = document.getElementById('photo-preview')
-      reader.onload = (event) => {
-        preview.src = event.currentTarget.result;
-        preview.style.padding = 0;
-      }
-      reader.readAsDataURL(input.files[0])
+  if (input.files && input.files[0]) {
+    const reader = new FileReader();
+    const preview = document.getElementById('photo-preview')
+    reader.onload = (event) => {
+      preview.src = event.currentTarget.result;
+      preview.style.padding = 0;
     }
+    reader.readAsDataURL(input.files[0])
+  }
 }
 
