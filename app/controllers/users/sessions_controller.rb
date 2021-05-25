d236f04dd8c386c770512112512484a5c5338e78
Nicolas Filzi <nfilzi.webservices@gmail.com>
2021-05-25 10:56:41 +0200

    Redirect user to new deal with targeted record after signin

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,9 +1,30 @@
 module Users
   class SessionsController < Devise::SessionsController
+    def new
+      session[:record_to_return_to_id] = params[:record_to_return_to_id].presence
+      super
+    end
+
     private
 
     def after_sign_in_path_for(resource)
-      myrecords_path
+      record_to_return_to_id = session.delete(:record_to_return_to_id)
+
+      if record_to_return_to_id
+        @record_to_return_to = Record.find_by(id: record_to_return_to_id)
+
+        if @record_to_return_to && record_not_owned_by_user?
+          new_record_deal_path(@record_to_return_to)
+        else
+          records_path
+        end
+      else
+        myrecords_path
+      end
+    end
+
+    def record_not_owned_by_user?
+      !current_user.records.include?(@record_to_return_to)
     end
   end
 end
